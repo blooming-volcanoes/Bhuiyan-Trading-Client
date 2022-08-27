75163a050c0df31ef65bb14ced3597951d9e02c1
Sadique <mhsadique1307@gmail.com>
2022-08-27 23:31:11 +0600

    category page made responsive

--- src/pages/Category/AllProducts/AllProducts.js
+++ src/pages/Category/AllProducts/AllProducts.js
@@ -7,7 +7,7 @@ const AllProducts = () => {
     for (let i = 0; i < 16; i++) {
         products.push(
             <div
-                className='bg-no-repeat bg-cover bg-center h-48 flex items-end'
+                className='bg-no-repeat bg-cover bg-center h-56 lg:h-52 flex items-end'
                 style={{
                     backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0.01) 70%, rgba(74, 74, 74, 100)), 
                         url(https://picsum.photos/id/${i + 1}/500/300`,
@@ -18,14 +18,14 @@ const AllProducts = () => {
     }
     return (
         <div className='container mx-auto'>
-            <div className=' flex justify-between'>
+            <div className='flex flex-col lg:flex-row justify-between items-center'>
                 {/* title and serch box */}
-                <div className='flex justify-end mb-4 items-center'>
+                <div className='flex justify-end mb-4 items-center pl-2 md:pl-0'>
                     <IoIosArrowDroprightCircle className='font-bold text-xl' />
                     <p className='font-bold text-lg ml-2'>All Products</p>
                 </div>
                 {/* search box */}
-                <div className='flex relative mb-10'>
+                <div className='flex relative mb-10 pr-2 md:pr-0'>
                     <input
                         className='w-80 border-slate-300 rounded-xl'
                         type="text"
@@ -33,12 +33,12 @@ const AllProducts = () => {
                         id=""
                         placeholder='Search products' />
                     <IoIosSearch
-                        className='font-bold text-xl absolute right-2 top-3'
+                        className='font-bold text-xl absolute right-4 lg:right-2 top-3'
                     />
                 </div>
             </div>
             {/* products */}
-            <div className='grid grid-cols-4 gap-y-7 gap-x-10 mb-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-3 md:gap-y-4 md:gap-x-5 lg:gap-y-7 gap-x-10 mb-10'>
                 {products}
             </div>
 
