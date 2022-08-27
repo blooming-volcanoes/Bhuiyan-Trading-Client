6528da1cb2d1f0336428fd2a357250fbc24f6da1
Sadique <mhsadique1307@gmail.com>
2022-08-27 19:49:30 +0600

    Category page done and Header modified

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import Footer from '../../Components/common/Footer/Footer';
 import Header from '../../Components/common/Header/Header';
+import { getAverageColor } from '../../lib/lib';
 import AllProducts from './AllProducts/AllProducts';
 import Banner from './Banner/Banner';
 import MostPopularProducts from './MostPopularProducts/MostPopularProducts';
 
 const Category = () => {
+
+    const image = useRef();
+    const [color, setColor] = useState(null);
+    const [imageUrl] = useState('https://picsum.photos/id/188/500/300')
+
+    useEffect(() => {
+        image.current.onload = () => {
+            const { R, G, B } = getAverageColor(image.current, 4);
+            setColor({ R, G, B });
+        }
+    }, []);
     return (
         <>
-            {/* <Header /> */}
-            <Banner />
+            <Header color={color} />
+            <Banner bannerImage={imageUrl} />
             <MostPopularProducts />
             <AllProducts />
+            <Footer />
+
+            {/* the image below is not shown on the UI, rather used to find the average color of the banner image */}
+            <img ref={image} src={imageUrl} crossOrigin='' alt='hello' className='hidden' />
         </>
     );
 };
---
+++ src/pages/Category/Sliders/Sliders.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/autoplay';
+
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+
+const Sliders = () => {
+    const slides = [];
+    for (let i = 0; i < 5; i++) {
+        slides.push(
+            <SwiperSlide>
+                <div
+                    className='bg-no-repeat bg-cover bg-center h-48 flex items-end'
+                    style={{
+                        backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0.01) 70%, rgba(74, 74, 74, 100)), 
+                        url(https://picsum.photos/id/${i + 1}/500/300`,
+                    }}>
+                    <p className='text-white font-bold text-xl ml-3 mb-3'>Bata Fish</p>
+                </div>
+            </SwiperSlide>
+        )
+    }
+    return (
+
+        <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            id='main'
+            navigation
+            pagination
+            spaceBetween={20}
+            slidesPerView={3}
+            autoplay
+
+        >
+            {slides}
+        </Swiper>
+
+    );
+};
+
+export default Sliders;
