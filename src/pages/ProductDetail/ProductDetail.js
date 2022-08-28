de7e197ca98f07020cb144997f4d1872d5853ee3
Sadique <mhsadique1307@gmail.com>
2022-08-28 20:15:37 +0600

    category page made fully responsive

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../../Components/common/Header/Header';
 
 const ProductDetail = () => {
     return (
-        <div>
-            ProductDetail
-        </div>
+        <>
+            <Header />
+            details
+        </>
     );
 };
 
