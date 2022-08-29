61c15ba43834178f30cefa0e701850859b85aaa8
Raiyan <tajkierhaque@gmail.com>
2022-08-29 17:26:42 +0600

    Large page half done :memo: :rocket:

---
+++ src/pages/ClientsPage/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import fish1 from "../../assets/Images/fish1.jpg";
+
+function Banner() {
+  return (
+    <div className="bg-top-bottom w-full bg-[#BCCEFF] bg-no-repeat py-10">
+    <div className="main-container grid lg:grid-cols-2">
+      {/* Left side */}
+      <h1 className="my-20 flex w-2/4 flex-col text-lg font-semibold leading-[1]">
+        Lorem ipsum <br />
+        <span className=" text-5xl">Lorem ipsum</span> <br />
+        <span className="ml-auto text-sm">Lorem ipsum</span>
+      </h1>
+
+      {/* Right Side */}
+      <div>
+        <img className="" src={fish1} alt="" />
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Banner
