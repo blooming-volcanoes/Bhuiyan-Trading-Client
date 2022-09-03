83e7071d21d8b3682ac0c4c6650bd235d4af349f
Sadique <mhsadique1307@gmail.com>
2022-09-03 21:37:48 +0600

    PdDetailsAndDescription's code replaced

--- src/pages/ProductDetail/PdDetailsAndDescription/PdDetailsAndDescription.js
+++ src/pages/ProductDetail/PdDetailsAndDescription/PdDetailsAndDescription.js
@@ -1,50 +1,39 @@
-import React from "react";
-import {
-  IoIosArrowDropleftCircle,
-  IoIosArrowDroprightCircle,
-} from "react-icons/io";
-import PdDescriptionAccordion from "../PdDescriptionAccordion/PdDescriptionAccordion";
+import React from 'react';
+import PdDescriptionAccordion from '../PdDescriptionAccordion/PdDescriptionAccordion';
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 
 const PdDetailsAndDescription = ({ bgImage }) => {
-  console.log(bgImage);
-  return (
-    <div
-      style={{
-        backgroundImage: `linear-gradient(to bottom, rgba(230, 230, 230, 0.8) 70%, rgba(230, 230, 230, 0.8)), 
+    console.log(bgImage);
+    return (
+        <div
+            style={{
+                backgroundImage: `linear-gradient(to bottom, rgba(230, 230, 230, 0.8) 70%, rgba(230, 230, 230, 0.8)), 
                 url(${bgImage})`,
-      }}
-      className="w-full bg-cover bg-center bg-no-repeat pt-5 pl-5 pr-5 lg:w-1/2"
-    >
-      <h1 className="mb-2 text-5xl font-bold">Red Fish</h1>
-      <h3 className="mb-5 text-xl font-bold">Category here</h3>
-      <p className="mb-3 font-bold">
-        Price: $ <span className="text-3xl font-bold text-red-600">800.00</span>
-        /unit
-      </p>
-      <p className="mb-10 text-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, velit
-        quae! Quae nisi ipsa quasi ea nobis dolore ex vero!
-      </p>
-      <PdDescriptionAccordion />
+            }}
+            className='w-full lg:w-1/2 pt-5 pl-5 pr-5 bg-no-repeat bg-cover bg-center'>
+            <h1 className='font-bold text-5xl mb-2'>Red Fish</h1>
+            <h3 className='text-xl font-bold mb-5'>Category here</h3>
+            <p className='font-bold mb-3'>Price: $ <span className='font-bold text-3xl text-red-600'>800.00</span>/unit</p>
+            <p className='text-2xl mb-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, velit quae! Quae nisi ipsa quasi ea nobis dolore ex vero!</p>
+            <PdDescriptionAccordion />
 
-      <div className="flex items-center justify-center">
-        <button className="mb-5 rounded-lg bg-red-900 px-3 py-2 text-lg font-bold text-white">
-          Contact us to purchase
-        </button>
-      </div>
+            <div className='flex justify-center items-center'>
+                <button className='mb-5 font-bold text-white text-lg px-3 py-2 bg-red-900 rounded-lg'>Contact us to purchase</button>
+            </div>
 
-      <div className="flex w-full gap-1">
-        <button className="flex h-20 w-1/2 items-center justify-center border-slate-500 bg-gray-100 hover:bg-gray-200">
-          <IoIosArrowDropleftCircle className="text-2xl lg:text-3xl" />
-          <span className="ml-1 text-lg  lg:text-2xl">Category Page</span>
-        </button>
-        <button className="flex h-20 w-1/2 items-center justify-center border-slate-500 bg-gray-100 hover:bg-gray-200">
-          <span className="mr-1 text-lg lg:text-2xl">Home Page</span>
-          <IoIosArrowDroprightCircle className="text-2xl lg:text-3xl" />
-        </button>
-      </div>
-    </div>
-  );
+            <div className='w-full flex gap-1'>
+                <button className='flex w-1/2 items-center justify-center bg-gray-100 h-20 hover:bg-gray-200 border-slate-500'>
+                    <IoIosArrowDropleftCircle className='text-2xl lg:text-3xl' />
+                    <span className='text-lg lg:text-2xl  ml-1'>Category Page</span>
+                </button>
+                <button className='flex w-1/2 items-center justify-center bg-gray-100 h-20 hover:bg-gray-200 border-slate-500'>
+                    <span className='text-lg lg:text-2xl mr-1'>Home Page</span>
+                    <IoIosArrowDroprightCircle className='text-2xl lg:text-3xl' />
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default PdDetailsAndDescription;
