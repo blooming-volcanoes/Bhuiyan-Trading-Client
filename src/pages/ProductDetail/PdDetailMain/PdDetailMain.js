e5a35e84964bfef5ffd25a032673209a7d55b16a
Sadique <mhsadique1307@gmail.com>
2022-09-03 21:36:09 +0600

    PdDetailMain's code replaced

--- src/pages/ProductDetail/PdDetailMain/PdDetailMain.js
+++ src/pages/ProductDetail/PdDetailMain/PdDetailMain.js
@@ -1,13 +1,16 @@
-import PdDetailsAndDescription from "../PdDetailsAndDescription/PdDetailsAndDescription";
-import PdImageSliders from "../PdImageSliders/PdImageSliders";
+import PdImageSliders from '../PdImageSliders/PdImageSliders';
+import PdDetailsAndDescription from '../PdDetailsAndDescription/PdDetailsAndDescription';
 
 const PdDetailMain = ({ images }) => {
-  return (
-    <div className="flex flex-col lg:flex-row">
-      <PdImageSliders bgImages={images} />
-      <PdDetailsAndDescription bgImage={images.primary} />
-    </div>
-  );
+
+    return (
+        <div className='flex flex-col lg:flex-row'>
+            <PdImageSliders bgImages={images} />
+            <PdDetailsAndDescription bgImage={images.primary} />
+
+
+        </div>
+    );
 };
 
 export default PdDetailMain;
