07ff6d032cfded96910167d98af6fc8bea1581ca
Raiyan <tajkierhaque@gmail.com>
2022-09-20 19:05:23 +0600

    Public routes added and major bug solved

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import "./styles/css/style.css";
 
 import "./Components/dashboard/charts/ChartjsConfig";
-import Home from "./pages/Homepage/Home";
 import AdminRoutes from "./routes/AdminRoutes";
+import PublicRoutes from "./routes/PublicRoutes";
 
 AOS.init();
 
@@ -26,7 +26,7 @@ function App() {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {/* Public Routes are here */}
-        <Route path="/*" element={<Home />} />
+        <Route path="/*" element={<PublicRoutes />} />
         {/* Admin Routes are here */}
         <Route path="/admin/dashboard/*" element={<AdminRoutes />} />
       </Routes>
---
+++ src/routes/PublicRoutes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "./../pages/Register/Register";
+const LatestNews = React.lazy(() => import("../pages/LatestNews/LatestNews"));
+const Category = React.lazy(() => import("../pages/Category/Category"));
+const Contact = React.lazy(() => import("../pages/Contact/Contact"));
+const ClientsPage = React.lazy(() =>
+  import("../pages/ClientsPage/ClientsPage")
+);
+const Home = React.lazy(() => import("../pages/Homepage/Home"));
+
+function PublicRoutes() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/latestNews" element={<LatestNews />} />
+      <Route path="/categories" element={<Category />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/ClientsPage" element={<ClientsPage />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+
+      {/* Dynamic pages */}
+
+      {/* <Route path="/blogPage" element={<BlogPage />} /> */}
+      {/* <Route path="/product-detail" element={<ProductDetail />} /> */}
+    </Routes>
+  );
+}
+
+export default PublicRoutes;
