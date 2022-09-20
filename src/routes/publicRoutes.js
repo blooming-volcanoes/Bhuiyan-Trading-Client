019514ea554846565d72d2f0a4bb6ca99daa261d
Raiyan <tajkierhaque@gmail.com>
2022-09-20 18:56:56 +0600

    Major error solved

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "./../pages/Register/Register";
+const LatestNews = React.lazy(() => import("../pages/LatestNews/LatestNews"));
+const Category = React.lazy(() => import("../pages/Category/Category"));
+const Contact = React.lazy(() => import("../pages/Contact/Contact"));
+const ClientsPage = React.lazy(() =>
+  import("../pages/ClientsPage/ClientsPage")
+);
+const Home = React.lazy(() => import("../pages/Homepage/Home"));
 
-function publicRoutes() {
-  return <div>publicRoutes</div>;
+function PublicRoutes() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/latestNews" element={<LatestNews />} />
+      <Route path="/categories" element={<Category />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/ClientsPage" element={<ClientsPage />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+
+      {/* Dynamic pages */}
+      {/* <Route path="/blogPage" element={<BlogPage />} /> */}
+      {/* <Route path="/product-detail" element={<ProductDetail />} /> */}
+    </Routes>
+  );
 }
 
-export default publicRoutes;
+export default PublicRoutes;
