c7916e0a3bffa6cb7218ac4e98b81501c8b0cc56
Raiyan <tajkierhaque@gmail.com>
2022-10-14 12:23:11 +0600

    Banner image has been fixed

--- src/pages/Homepage/Banner/Banner.js
+++ src/pages/Homepage/Banner/Banner.js
@@ -1,48 +1,60 @@
 import { useEffect, useState } from "react";
 // import BannerImg from "../../../assets/Images/bg.jpg";
-// import whiteBg from "../../../assets/Images/white-bg.jpg";
+import whiteBg from "../../../assets/Images/white-bg.jpg";
+import LoadingButton from "../../../Components/custom/Buttons/LoadingButton";
 import httpDashboardService from "./../../../services/dashboard.service";
 
 function Banner() {
   const [headerData, setHeaderData] = useState(null);
-
+  const [loader, setLoader] = useState(false);
   useEffect(() => {
     async function getHeaderData() {
+      setLoader(true);
       try {
         const data = await httpDashboardService.getHeaderData();
         setHeaderData(data);
       } catch (error) {
+        setLoader(false);
         console.log(error);
       }
+      setLoader(false);
     }
     getHeaderData();
   }, []);
 
   return (
-    <section
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-       url(${headerData ? headerData?.backgroundImg : ""})`,
-      }}
-      className="h-screen bg-cover bg-center bg-no-repeat"
-    >
-      <div className="main-container flex h-full items-center justify-center md:justify-start">
-        <div className="animate__animated animate__fadeInLeft text-center md:text-left">
-          <h3 className="text-2xl font-bold text-white md:text-4xl">
-            {headerData?.mainTitle || "Welcome"}
-          </h3>
-          <h1 className="text-[50px] font-bold text-white md:text-[70px]">
-            {headerData?.secondTitle || "Bhuiyan Trading"}
-          </h1>
-          <h3 className="text-2xl font-bold text-white md:text-4xl">
-            {headerData?.thirdTitle || "Corporation"}
-          </h3>
-          <button className="banner-btn" type="button">
-            Our Products
-          </button>
+    <>
+      {loader ? (
+        <div className="flex h-screen items-center justify-center space-y-4">
+          <LoadingButton styles="" svg="w-16 h-16 text-indigo-500" />
         </div>
-      </div>
-    </section>
+      ) : (
+        <section
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+       url(${headerData ? headerData?.backgroundImg : whiteBg})`,
+          }}
+          className="h-screen bg-cover bg-center bg-no-repeat"
+        >
+          <div className="main-container flex h-full items-center justify-center md:justify-start">
+            <div className="animate__animated animate__fadeInLeft text-center md:text-left">
+              <h3 className="text-2xl font-bold text-white md:text-4xl">
+                {headerData?.mainTitle || "Welcome"}
+              </h3>
+              <h1 className="text-[50px] font-bold text-white md:text-[70px]">
+                {headerData?.secondTitle || "Bhuiyan Trading"}
+              </h1>
+              <h3 className="text-2xl font-bold text-white md:text-4xl">
+                {headerData?.thirdTitle || "Corporation"}
+              </h3>
+              <button className="banner-btn" type="button">
+                Our Products
+              </button>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
