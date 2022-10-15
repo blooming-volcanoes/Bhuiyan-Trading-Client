c8c63839e5b1d11ae6eb98bb045832401462e5a0
Raiyan <tajkierhaque@gmail.com>
2022-10-15 19:51:35 +0600

    All static changes are done

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
       <div className="h-full min-h-[300px]">
         <iframe
           style={{ width: "100%", height: "100%", minHeight: "500px" }}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.5239599486977!2d91.82790081422624!3d22.333836447322092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8a842bd40ad%3A0xb5a685e4e6129d80!2sChittagong%20Railway%20Staion%2C%20Chittagong!5e0!3m2!1sen!2sbd!4v1661600276225!5m2!1sen!2sbd"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.4960316981224!2d91.82825471422629!3d22.33489184728412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30acd8a87343ffcf%3A0x4f44b97fb9ee0a9!2sMohiuddin%20Market!5e0!3m2!1sen!2sbd!4v1665841817709!5m2!1sen!2sbd"
           frameborder="0"
           scrolling="no"
           loading="Loading..."
