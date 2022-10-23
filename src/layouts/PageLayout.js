ee4ec91618ff42a0589f4a6350434759f3c714d4
Raiyan <tajkierhaque@gmail.com>
2022-10-23 12:04:19 +0600

    logo dynamic done

--- src/layouts/PageLayout.js
+++ src/layouts/PageLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "./../Components/common/Footer/Footer";
 import Header from "./../Components/common/Header/Header";
 
-function PageLayout({ children, color }) {
+function PageLayout({ children }) {
   return (
     <>
       <Header />
--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,4 +1,7 @@
-import PageLayout from "../../layouts/PageLayout";
+import { useEffect, useState } from "react";
+import Footer from "../../Components/common/Footer/Footer";
+import Header from "../../Components/common/Header/Header";
+import httpDashboardService from "../../services/dashboard.service";
 import Banner from "./Banner/Banner";
 import Future from "./Future/Future";
 import OurBlogs from "./OurBlogs/OurBlogs";
@@ -8,16 +11,35 @@ import ShipEquipment from "./ShipEquipment/ShipEquipment";
 import WhoWe from "./WhoWe/WhoWe";
 
 function Home() {
+  const [headerData, setHeaderData] = useState(null);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    async function getHeaderData() {
+      setLoader(true);
+      try {
+        const data = await httpDashboardService.getHeaderData();
+        setHeaderData(data);
+      } catch (error) {
+        setLoader(false);
+        console.log(error);
+      }
+      setLoader(false);
+    }
+    getHeaderData();
+  }, []);
   return (
-    <PageLayout>
-      <Banner />
+    <>
+      <Header />
+      <Banner headerData={headerData} loader={loader} />
       <OurProducts />
       <WhoWe />
       <Future />
       <ShipEquipment />
       <OurBlogs />
       <PopularProducts />
-    </PageLayout>
+      <Footer />
+    </>
   );
 }
 
